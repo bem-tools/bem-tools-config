b36b1870c88ebe1ca801121bb72b0bcf0e3d1742
Vladimir Grinenko <i@tadatuta.com>
2017-12-03 02:56:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const BemConfig = require('@bem/sdk.config');
 
 module.exports = function() {
+    const config = BemConfig();
+
     return Promise.all([
-        BemConfig().get(),
-        BemConfig().levelMap()
+        config.get(),
+        config.levelMap()
     ]).then(([all, levels]) => ({
         all,
         levels
